3208530d0fe88c633b80bc4cb38694a2e0a0ec32
MasamioNakada <mnakadab@unac.edu.pe>
2023-11-07 22:39:57 -0500

    fix_requirements

--- database.py
+++ database.py
@@ -6,14 +6,14 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-user = os.getenv("USER")
+user = os.getenv("DATABASEUSER")
 password = os.getenv("PASSWORD")
 server = os.getenv("SERVER")
 port = os.getenv("PORTS")
 database = os.getenv("DATABASE")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{user}:{password}@{server}:{port}/{database}"
-
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
